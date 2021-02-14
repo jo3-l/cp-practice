76bbd4b3a222bc2690f0650f4f676f05e3bc546e
jo3-l <ravenclawjoe101325@gmail.com>
2021-02-14 15:36:11 -0800

    feat(2012/*): j1-j4 solutions

---
+++ src/main/java/ccc_2012/J3.java
@@ -0,0 +1,26 @@
+package ccc_2012;
+
+import java.util.Scanner;
+
+public class J3 {
+    private static final char[][] INITIAL_GRID = {
+            {'*', 'x', '*'},
+            {' ', 'x', 'x'},
+            {'*', ' ', '*'},
+    };
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int scalingFactor = scanner.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3 * scalingFactor; i++) {
+            if (i != 0) sb.append("\n");
+            for (int j = 0; j < 3 * scalingFactor; j++) {
+                char c = INITIAL_GRID[i / scalingFactor][j / scalingFactor];
+                sb.append(c);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
---
+++ src/main/java/ccc_2012/J5.java
@@ -0,0 +1,7 @@
+package ccc_2012;
+
+public class J5 {
+    public static void main(String[] args) {
+
+    }
+}
