a0b487d01dcdf3d36df434f8e7634005ee419557
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-06-02 18:13:55 -0700

    feat(ccc2015): rewrite j5 using top-down dp for 15/15

--- src/main/java/ccc/ccc2015/J5.java
+++ src/main/java/ccc/ccc2015/J5.java
@@ -3,25 +3,29 @@ package ccc.ccc2015;
 import java.util.Scanner;
 
 public class J5 {
+    private static int[][][] dp;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int piecesOfPie = scanner.nextInt();
         int people = scanner.nextInt();
+        dp = new int[piecesOfPie + 1][people + 1][piecesOfPie + 1];
 
         System.out.println(ways(piecesOfPie, people, 0));
     }
 
     private static int ways(int piecesOfPie, int people, int lastPersonPieces) {
-        if (piecesOfPie < people) return 0;
-        if (people == 1 || people == piecesOfPie) return 1;
+        if (piecesOfPie < people) return dp[piecesOfPie][people][lastPersonPieces] = 0;
+        if (people == 1 || people == piecesOfPie) return dp[piecesOfPie][people][lastPersonPieces] = 1;
+        if (dp[piecesOfPie][people][lastPersonPieces] != 0) return dp[piecesOfPie][people][lastPersonPieces];
 
-        int maxPieFirstPerson = Math.floorDiv(piecesOfPie, people);
-        if (maxPieFirstPerson < lastPersonPieces) return 0;
+        int maxPieFirstPerson = piecesOfPie / people;
+        if (maxPieFirstPerson < lastPersonPieces) return dp[piecesOfPie][people][lastPersonPieces] = 0;
 
         int count = 0;
         int begin = lastPersonPieces == 0 ? 1 : lastPersonPieces;
-        for (int i = begin; i <= maxPieFirstPerson; i++) count += ways(piecesOfPie - i, people - 1, i);
+        for (int p = begin; p <= maxPieFirstPerson; p++) count += ways(piecesOfPie - p, people - 1, p);
 
-        return count;
+        return dp[piecesOfPie][people][lastPersonPieces] = count;
     }
 }
