c7b0fd2ea305676157abb114181db0433c91ba4e
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-06-21 09:11:04 -0700

    refactor(binarysearch/AnagramSubstrings): much faster impl

--- src/main/java/binarysearch/AnagramSubstrings.java
+++ src/main/java/binarysearch/AnagramSubstrings.java
@@ -1,34 +1,44 @@
 package binarysearch;
 
 public class AnagramSubstrings {
-    public int solve(String small, String big) {
-        if (big.length() < small.length()) return 0;
-        int d = 0;
-        int[] smallv = new int[26];
-        int[] mp = new int[26];
-        // populate initial values
-        for (int i = 0; i < small.length(); i++) {
-            mp[big.charAt(i) - 'a']++;
-            smallv[small.charAt(i) - 'a']++;
+    // xor this to see if all are eq
+    private final int allEqMask = 0b11111111111111111111111111;
+
+    public int solve(String s0, String s1) {
+        if (s1.length() < s0.length()) return 0;
+        int[] wantFreq = new int[26];
+        computeFreq(s0, wantFreq);
+
+        int count = 0;
+
+        int[] windowFreq = new int[26];
+        int same = 0;
+        for (int i = 0, j = s0.length() - 1; j < s1.length(); i++, j++) {
+            if (i == 0) {
+                computeFreq(s1.substring(i, j + 1), windowFreq);
+                for (int z = 0; z < 26; z++) {
+                    if (wantFreq[z] == windowFreq[z]) same |= 1 << z;
+                }
+                if ((same ^ allEqMask) == 0) count++;
+                continue;
+            }
+
+            int discard = s1.charAt(i - 1) - 'a';
+            if (--windowFreq[discard] != wantFreq[discard]) same &= ~(1 << discard); // unset bit
+            else same |= 1 << discard;
+
+            int add = s1.charAt(j) - 'a';
+            if (++windowFreq[add] != wantFreq[add]) same &= ~(1 << add); // unset bit
+            else same |= 1 << add;
+            if ((same ^ allEqMask) == 0) count++;
         }
 
-        int ret = 0;
-        if (areIntArraysEq(mp, smallv)) ret++;
-
-        // advance window
-        for (int i = small.length(); i < big.length(); i++) {
-            mp[big.charAt(i) - 'a']++;
-            mp[big.charAt(d++) - 'a']--;
-            if (areIntArraysEq(mp, smallv)) ret++;
-        }
-
-        return ret;
+        return count;
     }
 
-    private boolean areIntArraysEq(int[] arr0, int[] arr1) {
-        for (int i = 0; i < arr0.length; i++) {
-            if (arr0[i] != arr1[i]) return false;
+    private void computeFreq(String s, int[] buf) {
+        for (int i = 0; i < s.length(); i++) {
+            buf[s.charAt(i) - 'a']++;
         }
-        return true;
     }
 }
