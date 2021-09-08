b60398c47a81f5ba461ff83547c37d63b555c923
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-09-07 21:04:55 -0700

    refactor(binarysearch): clean up code

--- src/main/cpp/binarysearch/sinking_islands.cpp
+++ src/main/cpp/binarysearch/sinking_islands.cpp
@@ -2,50 +2,36 @@
 
 using namespace std;
 
-int di[4] = {1, -1, 0, 0};
-int dj[4] = {0, 0, 1, -1};
+const int di[] = {1, -1, 0, 0};
+const int dj[] = {0, 0, 1, -1};
 
 vector<vector<int>> solve(vector<vector<int>> &board) {
 	int R = (int)board.size();
 	int C = (int)board[0].size();
-
-	function<void(int, int, int)> color;
-	color = [&](int i, int j, int c) {
+	function<void(int, int, int)> color_land = [&](int i, int j, int c) {
+		board[i][j] = c;
 		for (int d = 0; d < 4; d++) {
 			int ni = i + di[d], nj = j + dj[d];
 			if (0 <= ni && ni < R && 0 <= nj && nj < C &&
-			    board[ni][nj] == 1) {
-				board[ni][nj] = c;
-				color(ni, nj, c);
-			}
+			    board[ni][nj] == 1)
+				color_land(ni, nj, c);
 		}
 	};
 
 	for (int i = 0; i < R; i++) {
 		if (i == 0 || i == R - 1) {
-			for (int j = 0; j < C; j++)
-				if (board[i][j] == 1) {
-					board[i][j] = -1;
-					color(i, j, -1);
-				}
+			for (int j = 0; j < C; j++) {
+				if (board[i][j] == 1) color_land(i, j, -1);
+			}
 		} else {
-			if (board[i][0] == 1) {
-				board[i][0] = -1;
-				color(i, 0, -1);
-			}
-			if (board[i][C - 1] == 1) {
-				board[i][C - 1] = -1;
-				color(i, C - 1, -1);
-			}
+			if (board[i][0] == 1) color_land(i, 0, -1);
+			if (board[i][C - 1] == 1) color_land(i, C - 1, -1);
 		}
 	}
 
 	for (int i = 0; i < R; i++) {
 		for (int j = 0; j < C; j++) {
-			if (board[i][j] == 1) {
-				board[i][j] = 0;
-				color(i, j, 0);
-			}
+			if (board[i][j] == 1) color_land(i, j, 0);
 		}
 	}
 
